edf9da8c4814eb95d0f6200217d3ea137414878c
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2019-03-01 22:56:33 +0100

    added readme, install dependencies 📦

--- t.js
+++
@@ -1,6 +0,0 @@
-const { SendGallery } = require('./index');
-
-(async () => {
-  const t = await SendGallery('This is a gallery', 'sdkfjdkfkdjfdkfjdkfjkdjfkdfjdkfadksojfpksdjopeifeojfiffffffffffffffffffffffffffffffffffffff', 'img_url', {type: 'url', title: 'fgkfjgkfjg', value: 'url-here'})
-  console.log(t)
-})()
